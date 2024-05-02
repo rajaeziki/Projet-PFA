57bed48d46c96a3db4ab0d0bea4482e066af0284
Majjiee <128259183+Majjiee@users.noreply.github.com>
2024-05-02 22:06:16 +0100

    Update myFirstPyProject.py
    added alerte: send a msg in whatsapp and upload the captured picture in firebase

--- myFirstPyProject.py
+++ myFirstPyProject.py
@@ -8,6 +8,9 @@ import cv2
 import os
 import face_recognition as fr
 import cvzone
+import pywhatkit
+from datetime import datetime
+import time
 cred=credentials.Certificate("C:\\Users\\ULTRA PC\\Downloads\\pfa1-3687a-firebase-adminsdk-hkao1-2db974ef46.json")
 
 firebase_admin.initialize_app(cred,{
@@ -20,6 +23,7 @@ cap.set(4,480)
 MyFoldimg='C:\\Users\\ULTRA PC\\source\\repos\\myFirstPyProject\\images\\'
 imgpathlist=os.listdir(MyFoldimg)
 MyfoldList=[]
+os.environ['num'] = '+212658773297'
 for el in imgpathlist:
     MyfoldList.append(cv2.imread(os.path.join(MyFoldimg,el)))
 print('load encoding file')
@@ -48,5 +52,20 @@ while True:
         y1,x2,y2,x1=y1*4,x2*4,y2*4,x1*4
         bbox = (x1 - 10, y1 - 10, x2-x1, y2-y1)
         img=cvzone.cornerRect(img,bbox,rt=0)
+        if True in matches:
+            matches_found = True
+    if not matches:
+        now=datetime.now()
+        hr=int(now.strftime("%H"))
+        min=int(now.strftime("%M"))
+        num=os.environ.get('num')
+        try:
+            pywhatkit.sendwhatmsg_instantly(num, "Hi maj, an unknown person is here")
+            print("msg sent successfully!")
+            capture_image()
+        except Exception as e:
+            print(f"Error sending msg")
+            time.sleep(10)
+            
     cv2.imshow("webcam",img)
     cv2.waitKey(1)
